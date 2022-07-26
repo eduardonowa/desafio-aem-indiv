3aa7d9f08463e79b73fabed3e35287053602f805
Eduardo Nowakoski <eduardo_nowa@hotmail.com>
2022-07-25 21:03:39 -0300

    feat: adding button component

---
+++ ui.frontend/src/components/micro/ButtonType/ButtonType.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button>{{ buttonText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonType',
+  props: {
+    buttonText: {
+      type: String,
+      default: 'back to homepage'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scooped>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+button {
+  width: 216px;
+  height: 68px;
+  background: #333333;
+  color: #fff;
+  font-family: 'Space Mono', monospace;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 21px;
+  letter-spacing: -0.035em;
+  text-transform: uppercase;
+}
+</style>
